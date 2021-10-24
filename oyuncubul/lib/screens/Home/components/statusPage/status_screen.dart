e39eddcdc23c13431ef20af7eca5f9c2f8a16a42
omerfaruksahin <83252984+engineerOmer@users.noreply.github.com>
2021-10-24 14:26:34 +0300

    Home-StatusPage eklendi

---
+++ oyuncubul/lib/screens/Home/components/statusPage/status_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:oyuncubul/screens/Home/components/statusPage/components/status_body.dart';
+
+class StatusScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
