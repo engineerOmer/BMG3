6f5e1fcd5bb7d638c4d59dd1e94134e6ac1bfa42
omerfaruksahin <83252984+engineerOmer@users.noreply.github.com>
2021-10-31 17:01:19 +0300

    son

---
+++ oyuncubul/lib/screens/Home/components/profilPage/FeedBack/feedback_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:oyuncubul/screens/Home/components/profilPage/FeedBack/components/feedback_body.dart';
+
+//'package:oyuncubul/screens/Home/components/profilePage/FeedBack/components/feedback_body.dart';
+
+class FeedBack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
